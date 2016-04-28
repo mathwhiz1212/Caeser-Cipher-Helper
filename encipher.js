96a21ecdcee615ca5c60f8409acdc5803b2b81b7
Joshua Mudge <mathwhiz1212@users.noreply.github.com>
2016-04-27 18:03:43 -0600

    Mr. Oneal's code.

--- encipher.js
+++ encipher.js
@@ -1,84 +1,114 @@
-"use strict";
+'use strict';
 //Licensed under the Apache License 2.0. See license file for information.
 
-function encipher() {
-  var il = "0";
-  var alphabet = "";
-  var userInput = "";
-  var shiftNum = "";
-  var LetterReg = /^[A-Za-z]+$/;
+function sanitizeInputs(opts) {
+  var letterReg = /^[A-Za-z]+$/;
   var numReg = /^\d+$/;
-  var Answer = "";
-  var spaceReg = /\s/;
+
+  //Checks if input is a letter. Uses a modified version of code found here: http://www.w3resource.com/javascript/form/all-letters-field.php
+
+  // was userInput.match(letterReg)
+  if (!letterReg.test(opts.input)) {
+    //Tells them to enter only letters with no caps, spaces or punctuation.
+    // TODO ignore non-alphas letters in the loop
+
+    throw new Error("Please enter only letters, no caps, spaces or punctuation in message field.");
+  }
+
+  // was shiftNum.match(numReg)
+  if (!numReg.test(opts.shift)) {
+    //Tells them to enter only numbers.
+
+    throw new Error("Please enter only numbers in the shift field.");
+  }
+
+  // NOTE
+  // in JavaScript you must always use parseInt(str, radix)
+  // where radix is the base - 2 (binary), 10 (decimal), 16 (hex), etc
+  // DO NOT use Number()
+  // it is sometimes wrong, for example: Number("017")
+  // it should only be used if you know you're parsing scientific notation i.e. Number("2.7e6")
+  return {
+    input: opts.input
+  , shift: parseInt(opts.shift, 10)
+  };
+}
+
+function cipher(opts) {
+  var userInput = opts.input;
+  var shiftNum = opts.shift;
+
+  var il;
+  var alphabet = "";
+  var answer = "";
   var inputLength = "";
   var inputSplit = "";
-  var i = "0";
-  
-  //Get input
-  
-  var userInput = document.getElementById("userInput").value;
-  var shiftNum = document.getElementById("shiftNum").value;
-  
-  //Checks if input is a letter. Uses a modified version of code found here: http://www.w3resource.com/javascript/form/all-letters-field.php
-  
-  if (userInput.match(LetterReg))
-  {
-    //Do nothing.
-  }
-  else
-  {
-    //Tells them to enter only letters with no caps, spaces or punctuation.
-    
-    alert("Please enter only letters, no caps, spaces or punctuation in message field.");
-  }
-  
-  if (shiftNum.match(numReg))
-  {
-    //Do nothing.
-  }
-  else
-  {
-    //Tells them to enter only numbers.
-    
-    alert("Please enter only numbers in the shift field.");
-  }
-  
+  var i;
+
   //Gets input length.
-  
-  var inputLength = userInput.length;
-  
-  
+  inputLength = userInput.length;
+
+
   //Splits input into an array.
-  
-  var inputSplit = userInput.split("");
-  
+  inputSplit = userInput.split("");
+
   //Code using Evan's idea and his fix.
-  
-  alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  
-  for (il = 0; il < inputLength; il++) {
-    
-    for (i = 0; i < 26; i++) {
-      
+  alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  // TODO create alphabet table
+
+  for (il = 0; il < inputLength; il += 1) {
+
+    for (i = 0; i < alphabet.length; i += 1) {
+
       if (inputSplit[il] === alphabet[i]) {
-        
+
         //Debug code from Evan is commented out.
         //console.log(inputSplit[il]);
         //console.log(i);
-        
-        var letter = Number(i) + Number(shiftNum);
+
+        var letter = i + shiftNum;
+        // TODO use mod operator %
         if (letter > 25) {
-          var letter = letter - 26;
+          // letter = letter - 26;
+          letter -= 26;
         }
-        
-        var Answer = Answer + alphabet[letter];
+
+        //answer = answer + alphabet[letter];
+        answer += alphabet[letter];
+
+        // we are done with the inner alphabet loop
         break;
       }
     }
   }
-  
-  //Show result
-  alert("Your secret message is " +  Answer);
-  document.getElementById("Answer").innerHTML = "Your secret message is " + Answer;
-  
+
+  return answer;
 }
+
+function runInBrowser() {
+  var options = sanitizeInputs({
+    input: document.getElementById("userInput").value
+  , shift: document.getElementById("shiftNum").value
+  });
+  var cipherText;
+
+  try {
+    cipherText = cipher(options);
+  } catch(e) {
+    window.alert(e.message);
+  }
+
+  // Show result
+  window.alert("Your secret message is " +  cipherText);
+  document.getElementById("Answer").innerHTML = "Your secret message is " + cipherText;
+}
+
+function runInNode() {
+  var answer = cipher({ input: "HelloWorldItIsSuchAGoodLongLongLongDayTodayDontYouThink", shift: 7 });
+
+  console.log(answer);
+}
+
+runInNode();
+
